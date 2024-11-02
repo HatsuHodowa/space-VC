10c18c56a1ef1ee2f0961943d6437cf0e5268246
Mustafa Aljumayli <idmustafase@gmail.com>
2024-11-02 13:09:48 -0400

    fnished basic models

---
+++ models/Liability.py
@@ -0,0 +1,15 @@
+class Liability:
+
+    def __init__(self, debt_amount, interest_rate) -> None:
+        self.debt_amount = debt_amount
+        self.interest_rate = interest_rate
+    
+    def pay_loan(self, amount):
+        self.debt_amount -= amount
+        return self.debt_amount
+    
+    def calculate_interest(self):
+        self.debt_amount += self.debt_amount * self.interest_rate
+        return self.debt_amount
+    
+    
