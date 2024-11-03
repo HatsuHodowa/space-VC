886bf3dac3aa2fab5a86ccaabf56d3756e27bda9
Mustafa Aljumayli <idmustafase@gmail.com>
2024-11-03 00:18:13 -0400

    added tax

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 from models.Asset import Asset, AssetType
 from models.Liability import Liability
 from models.Game import Game
+from models.Job import Job
 
 game = Game()
 liability = Liability("Car", 25000, 0.06, 12)
-asset2 = Asset("Risk", 50000, 30000, 0, 0.04, AssetType.PROPERTY, 5)
+asset2 = Asset("House", 500000, 0, 0.05, 0.01, 500000, AssetType.PROPERTY)
+job = Job("Software Engineer", 1000000)
 
-game.player.buy_asset(asset2)
-print(game.game_status())
+game.player.get_job(job)
 game.monthly_update()
-print(game.game_status())
+game.monthly_update()
+game.monthly_update()
+print(game.player.buy_asset(asset2))
+game.monthly_update()
+game.monthly_update()
+game.monthly_update()
+game.player.calculate_all_taxes()
+game.game_status()
