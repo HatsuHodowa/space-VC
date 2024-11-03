9e3fe731621db20fd2aa78be7674327be3744eba
ni438cn <97555198+ni438cn@users.noreply.github.com>
2024-11-03 09:46:14 -0500

    fix

--- models/Catastrophy.py
+++ models/Catastrophy.py
@@ -2,13 +2,13 @@ import random
 
 
 class Catastrophy:
-    def __init__(self, probability = 0.02):
+    def __init__(self, probability = 0.01):
         self.probability = probability
         
     def impact(self, value):
         
         if random.random()/value < self.probability :
             print("catastrophy occurred!")
-            return random.random()
+            return random.random()*0.5+0.5
         else:
             return 1
